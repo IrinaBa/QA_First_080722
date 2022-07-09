ac845113c78d4a00a4e244337de0a8044ce3f0a5
Iryna Bartz <1208ein@mail.ru>
2022-07-09 23:54:14 +0200

    Added first element

---
+++ src/test/java/com/telran/qa/FirstSeleniumTest.java
@@ -0,0 +1,37 @@
+package com.telran.qa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.beans.BeanProperty;
+
+public class FirstSeleniumTest {
+    WebDriver driver;
+    //befor - setUp
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+
+    }
+    @Test
+    public void openGoogleTest(){
+        System.out.println("Site opened!");
+
+            }
+
+            @Test
+    public void navigateTest(){
+//go back
+        driver.navigate().back();
+        // go forward
+        driver.navigate().forward();
+        //refresh the page
+        driver.navigate().refresh();
+            }
+    //Test
+    //after-tearDawn
+
+}
